f7a5510522e5acae8be58a550964c9cc58ac24e4
Hrishikesh <hvaidya072@gmail.com>
2019-03-17 04:57:24 +0530

    this is sad

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
-from flask import Flask, request
+from flask import Flask, request, send_file
 from flask import render_template
 from script import extract_text
 from werkzeug import secure_filename
 from pdf_decode import pdf_to_text
+from str_space import remove_space
+from image import img_to_txt
 
 app = Flask(__name__)
 
 texts = extract_text()
 
-@app.route('/')
-def index():
-    return render_template('home.html')
-
+# @app.route('/')
+# def index():
+#     return render_template('home.html')
+global file_name
 @app.route('/about')
 def about():
     return render_template('about.html')
@@ -20,19 +22,30 @@ def about():
 def articles():
     return render_template('articles.html',text=texts)
 
-@app.route('/upload')
+@app.route('/')
 def upload():
-    return render_template('upload_file.html')
+    return render_template('home.html')
 
 @app.route('/uploader', methods = ['GET', 'POST'])
 def upload_file():
+   global file_name
    if request.method == 'POST':
       f = request.files['file']
+      file_name = f.filename
       f.save('data/' + secure_filename(f.filename))
-      pdf_to_text(f.filename)
-      return 'file uploaded and converted successfully'
+      if '.pdf' in f.filename:
+         pdf_to_text(f.filename)
+      elif '.png' in f.filename or '.gif' in f.filename:
+         img_to_txt(f.filename)
+      else:
+         return 'invalid file type'
+      return render_template('download.html')
+
+@app.route('/downloader')
+def downloader():
+   return send_file('C:/Users/Himanshu Prajapati/Downloads/Sampleproject/converted/'+ file_name[:-4]+'.txt')
+
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
