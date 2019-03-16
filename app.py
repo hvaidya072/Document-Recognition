26752df3eeb894303579317bf640f84a88c3d8d7
Hrishikesh <hvaidya072@gmail.com>
2019-03-16 20:03:24 +0530

    commit lol

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 from script import extract_text
+from werkzeug import secure_filename
+from pdf_decode import pdf_to_text
 
 app = Flask(__name__)
 
@@ -17,6 +19,19 @@ def about():
 @app.route('/articles')
 def articles():
     return render_template('articles.html',text=texts)
+
+@app.route('/upload')
+def upload():
+    return render_template('upload_file.html')
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save('data/' + secure_filename(f.filename))
+      pdf_to_text(f.filename)
+      return 'file uploaded and converted successfully'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
